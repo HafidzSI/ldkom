be158eb67116c48452f1c1577047448ba3bd62a6
HafidzSI <crozzgazelle4@gmail.com>
2019-11-14 16:03:17 +0700

    commit -1
    commit -1

---
+++ resources/views/print/detail.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.main')
+
+@section('title','Detail transaksi')
+
+@section('content')
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/print','PrintController@index')->name('print.index');
+Route::get('/print/tambah','PrintController@tambah')->name('print.tambah');
+Route::post('/print/simpan','PrintController@simpan')->name('print.simpan');
+Route::get('/print/{id}/edit','PrintController@edit')->name('print.edit');
+Route::get('/print/{id}/detail','PrintController@detail')->name('print.detail');
